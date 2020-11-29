db7cca2833469a2319ceb36fc8feb0d93065a0a3
a2shiyari <a2shiyari@gmail.com>
2020-11-29 16:29:47 +0330

    Add support for game events

---
+++ Tetris.Game/GameEvent.cs
@@ -0,0 +1,30 @@
+﻿namespace Tetris.Game
+{
+    /// <summary>
+    /// This enum indicates the event that occures in the game
+    /// </summary>
+    public enum GameEvent
+    {
+        MoveRightSuccessful,
+        MoveRightFailed,
+        MoveLeftSuccessful,
+        MoveLeftFailed,
+        RotationSuccessful,
+        RotationFailed,
+        HoldSuccessful,
+        HoldFailed,
+        TetrominoTouchDown,
+        TetrominoLocked,
+        OneLineCleared,
+        TwoLinesCleared,
+        ThreeLinesCleared,
+        FourLinesCleared,
+        HardDrop,
+        HardDropWithOneLineClear,
+        HardDropWithTwoLinesClear,
+        HardDropWithThreeLinesClear,
+        HardDropWithFourLinesClear,
+        LevelUp,
+        PerfectClear
+    }
+}
---
+++ Tetris.Game/GameEventsEventArgs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Tetris.Game
+{
+    /// <summary>
+    /// Game Events Event Args class is used for game events event handler
+    /// </summary>
+    public class GameEventsEventArgs : EventArgs
+    {
+
+        #region Ctor
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="gameEvent"></param>
+        public GameEventsEventArgs(GameEvent gameEvent)
+        {
+            Event = gameEvent;
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Occured event in the game
+        /// </summary>
+        public GameEvent Event { get; set; } 
+
+        #endregion
+
+    }
+}
